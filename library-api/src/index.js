141b4883f1ac4dca99844506aa73f02d8fff2ab4
Geovanna <geovannaterceros@gmail.com>
2026-06-12 21:50:11 -0400

    Fourth commit: create project backend with express.

---
+++ library-api/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API Express funcionando correctamente' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en http://localhost:${PORT}`);
+});
